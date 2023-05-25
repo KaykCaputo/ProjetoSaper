210e695d918e3a1f6fd9419f1183f734698b28e6
Kayk Caputo <kayk.caputo@gmail.com>
2023-05-25 20:48:19 -0300

    Initialization of the spring project and creation of the entities base

---
+++ src/main/java/com/example/sl/models/bed.java
@@ -0,0 +1,44 @@
+package com.example.sl.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class bed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long bed_id;
+    Boolean occupied;
+    String type;
+
+    public bed() {
+    }
+
+    public bed(Long bed_id, Boolean occupied, String type) {
+        this.bed_id = bed_id;
+        this.occupied = occupied;
+        this.type = type;
+    }
+
+    public Boolean getOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(Boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getBed_id() {
+        return bed_id;
+    }
+}
