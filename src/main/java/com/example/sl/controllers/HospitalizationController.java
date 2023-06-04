61e4845051c30d27f06876a586579e28ff938be5
joaovs2004 <joaovitorschmittschmitt@gmail.com>
2023-06-04 20:57:57 -0300

    Add HospitalizationController

---
+++ src/main/java/com/example/sl/controllers/HospitalizationController.java
@@ -0,0 +1,38 @@
+package com.example.sl.controllers;
+
+import com.example.sl.dto.hospitalization.HospitalizationRequestDTO;
+import com.example.sl.services.HospitalizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hospitalization")
+public class HospitalizationController {
+    @Autowired
+    HospitalizationService hospitalizationService;
+
+    @GetMapping
+    public Object getHospitalizations() {
+        return hospitalizationService.getAllHospitalizations();
+    }
+
+    @GetMapping("/{id}")
+    public Object getHospitalizationById(@PathVariable(name = "id") Long id) {
+        return hospitalizationService.getHospitalizationById(id);
+    }
+
+    @PostMapping
+    public Object addHospitalization(@RequestBody HospitalizationRequestDTO hospitalizationRequestDTO) {
+        return hospitalizationService.addHospitalization(hospitalizationRequestDTO);
+    }
+
+    @PutMapping("/{id}")
+    public Object updateHospitalization(@PathVariable(name = "id") Long id, @RequestBody HospitalizationRequestDTO hospitalizationRequestDTO) {
+        return hospitalizationService.updateHospitalization(id, hospitalizationRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public Object deleteHospitalization(@PathVariable(name = "id") Long id) {
+        return hospitalizationService.deleteHospitalization(id);
+    }
+}
