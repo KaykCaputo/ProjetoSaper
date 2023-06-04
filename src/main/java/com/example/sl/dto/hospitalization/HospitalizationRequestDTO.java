40191cd01eaba8defffcb8cdcfdd02a4a76600cc
joaovs2004 <joaovitorschmittschmitt@gmail.com>
2023-06-04 20:57:14 -0300

    Add Hospitalization DTOs

---
+++ src/main/java/com/example/sl/dto/hospitalization/HospitalizationRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.sl.dto.hospitalization;
+
+import java.util.Date;
+
+public class HospitalizationRequestDTO {
+    public Long medicalRecord_id;
+    public Long bed_id;
+    public String fugilin;
+    public String origem;
+    public Date hospitalizationDate;
+    public String permanenceReason;
+    public String specialty;
+}
---
+++ src/main/java/com/example/sl/dto/hospitalization/HospitalizationResponseDTO.java
@@ -0,0 +1,27 @@
+package com.example.sl.dto.hospitalization;
+
+import com.example.sl.models.Hospitalization;
+
+import java.util.Date;
+
+public class HospitalizationResponseDTO {
+    public Long hospitalization_id;
+    public Long medicalRecord_id;
+    public Long bed_id;
+    public String fugilin;
+    public String origem;
+    public Date hospitalizationDate;
+    public String permanenceReason;
+    public String specialty;
+
+    public HospitalizationResponseDTO(Hospitalization hospitalization) {
+        this.hospitalization_id = hospitalization.getHospitalization_id();
+        this.medicalRecord_id = hospitalization.getMedicalRecord_id();
+        this.bed_id = hospitalization.getBed_id();
+        this.fugilin = hospitalization.getFugilin();
+        this.origem = hospitalization.getOrigem();
+        this.hospitalizationDate = hospitalization.getHospitalizationDate();
+        this.permanenceReason = hospitalization.getPermanenceReason();
+        this.specialty = hospitalization.getSpecialty();
+    }
+}
