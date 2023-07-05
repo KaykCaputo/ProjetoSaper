6e88f887dee5e4382dea85a713b19364b58c1a7c
Kayk Caputo <kayk.caputo@gmail.com>
2023-07-05 11:22:44 -0300

    fixes

--- src/main/java/com/example/sl/services/UserService.java
+++ src/main/java/com/example/sl/services/UserService.java
@@ -2,8 +2,8 @@ package com.example.sl.services;
 
 import com.example.sl.dto.user.UserRequestDTO;
 import com.example.sl.dto.user.UserResponseDTO;
-import com.example.sl.models.User;
-import com.example.sl.repository.UserRepository;
+import com.example.sl.models.Users;
+import com.example.sl.repository.UsersRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,39 +15,38 @@ import java.util.Optional;
 @Service
 public class UserService {
     @Autowired
-    UserRepository userRepository;
+    UsersRepository usersRepository;
 
     public Object getAllByName(String name){
         if(name.equals("")) {
-            return userRepository.findAll().stream().map((UserResponseDTO::new));
+            return usersRepository.findAll().stream().map((UserResponseDTO::new));
         }else{
-            return userRepository.findAllByNameContaining(name).stream().map((UserResponseDTO::new));
+            return usersRepository.findAllByUsernameContaining(name).stream().map((UserResponseDTO::new));
         }
     }
 
     @Transactional
     public Object save(UserRequestDTO userRequestDTO){
-        User user = new User(userRequestDTO);
+        Users user = new Users(userRequestDTO);
 
-        user = userRepository.save(user);
-
-        return new UserResponseDTO(user);
+        usersRepository.save(user);
+        return user;
     }
 
     public ResponseEntity<Object> findById(Long id) {
-        Optional<User> userOptional = userRepository.findById(id);
+        Optional<Users> userOptional = usersRepository.findById(id);
 
         return userOptional.<ResponseEntity<Object>>map(user -> ResponseEntity.status(HttpStatus.OK).body(new UserResponseDTO(user))).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found"));
     }
 
     @Transactional
     public Object update(Long id, UserRequestDTO userRequestDTO) {
-        Optional<User> userOptional = userRepository.findById(id);
+        Optional<Users> userOptional = usersRepository.findById(id);
 
         if(userOptional.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
         }else{
-            User user= userOptional.get();
+            Users user= userOptional.get();
 
             if(userRequestDTO.password!=null) {
                 user.setPassword(userRequestDTO.password);
@@ -57,15 +56,15 @@ public class UserService {
                 user.setUsername(userRequestDTO.username);
             }
 
-            return ResponseEntity.status(HttpStatus.OK).body(new UserResponseDTO(userRepository.save(user)));
+            return ResponseEntity.status(HttpStatus.OK).body(new UserResponseDTO(usersRepository.save(user)));
         }
     }
 
     @Transactional
     public ResponseEntity<Object> delete(Long id) {
-        Optional<User> userOptional = userRepository.findById(id);
+        Optional<Users> userOptional = usersRepository.findById(id);
 
-        userRepository.delete(userOptional.get());
+        usersRepository.delete(userOptional.get());
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
