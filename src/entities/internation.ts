e87ac0344006c324359e7a6d66853bef2bae89ff
joaovs2004 <joaovitorschmittschmitt@gmail.com>
2023-07-07 18:22:25 -0300

    Add internation and pacient entities

---
+++ src/entities/internation.ts
@@ -0,0 +1,10 @@
+export default interface Internation {
+    hospitalization_id: number,
+    medicalRecord_id: number,
+    bed_id: number,
+    fugilin: string,
+    origem: string,
+    hospitalizationDate: string,
+    permanenceReason: string,
+    specialty: string
+}
---
+++ src/entities/pacient.ts
@@ -0,0 +1,8 @@
+export default interface Pacient {
+    medicalRecord_id: number,
+	cpf: string,
+	name: string,
+    birthday: string,
+    phone: string,
+    medicalRelease: boolean
+}
